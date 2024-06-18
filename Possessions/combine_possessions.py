679300702d8ef1467c0f8087ca85c658f97bbb3a
Brandon Michaud <michaudbrandond@gmail.com>
2024-06-18 13:06:05 -0500

    Added game date to season-long possessions

--- Possessions/combine_possessions.py
+++ Possessions/combine_possessions.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 # Reformat a single game possessions to fit style of RAPM calculator
-def reformat_single_game_possessions(game_possessions):
+def reformat_single_game_possessions(game_possessions, date):
     # Reformat each row at a time
     reformatted_possessions = []
     for _, possession in game_possessions.iterrows():
@@ -26,7 +26,7 @@ def reformat_single_game_possessions(game_possessions):
 
         # Create a row with offensive players, defensive players, points score in possessions, and number of possessions
         # which is always 1 since we are using possession data instead of stints
-        reformatted_possessions.append(offensive_players + defensive_players + [offensive_points, 1])
+        reformatted_possessions.append(offensive_players + defensive_players + [offensive_points, 1, date])
 
     # Create a data frame with the reformatted possessions
     reformatted_possessions_df = pd.DataFrame(reformatted_possessions)
@@ -34,7 +34,7 @@ def reformat_single_game_possessions(game_possessions):
                                           'offensive_player4', 'offensive_player5',
                                           'defensive_player1', 'defensive_player2', 'defensive_player3',
                                           'defensive_player4', 'defensive_player5',
-                                          'points', 'possessions']
+                                          'points', 'possessions', 'date']
 
     return reformatted_possessions_df
 
@@ -46,19 +46,22 @@ def combine_season_possessions(season, season_type, schedule_filename, possessio
     schedule = pd.read_csv(schedule_filename.format(season, season_type), dtype=str)
 
     # Extract the game IDs from the schedule
-    game_ids = schedule['GAME_ID'].unique()
-    n_games = len(game_ids)
+    games = schedule[['GAME_ID', 'GAME_DATE']].drop_duplicates()
+    games.reset_index(drop=True, inplace=True)
+    n_games = games.shape[0]
 
     # Get possessions for each game in the season
     season_possessions = []
-    for i, game_id in enumerate(game_ids):
+    for i, game in games.iterrows():
+        game_id = game['GAME_ID']
+        game_date = game['GAME_DATE']
         print(f'{((i + 1) / n_games):.2%} {season} {season_type}: {game_id}')
 
         # Get the possessions for this game
         game_possessions = pd.read_csv(possessions_filename.format(game_id))
 
         # Reformat the possessions
-        reformatted_possessions = reformat_single_game_possessions(game_possessions)
+        reformatted_possessions = reformat_single_game_possessions(game_possessions, game_date)
 
         # Add this game's reformatted possessions to season-long list
         season_possessions.append(reformatted_possessions)
